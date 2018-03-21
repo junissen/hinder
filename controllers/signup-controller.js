6f51560f3b549025ecc27e9e91c2eb69117014eb
Julia Nissen <nisse053@umn.edu>
2018-03-21 16:14:12 -0500

    user creation finished

--- controllers/signup-controller.js
+++ controllers/signup-controller.js
@@ -25,7 +25,43 @@ module.exports = function(app) {
 
     });
 
+    app.get("/api/user/create", function(req, res) {
+    	db.user.findAll({
+			where: {
+				user_name: req.query.user_name
+			}
+		}).then(function(result) {
 
+			if(result.length == 0) {
+				var textObjectNone = {
+					message: 'No user found'
+				} 
+				res.json({textObjectNone})
+			}
+
+			else {
+				var textObjectFound = {
+					message: 'This username has already been taken'
+				} 
+				res.json({textObjectFound})
+			}
+		});
+    });
+
+    app.post("/api/user/create", function(req, res) {
+
+    	db.user.create({
+    		"user_name": req.body.user_name,
+    		"password": req.body.password,
+    		"photo": req.body.photo,
+    		"phone_number": req.body.phone_number,
+    		"phone_carrier": req.body.phone_carrier,
+    		"group_id": req.body.group_id
+    	}).then(function(result) {
+    		res.json(result)
+    	})
+
+    });
 
 }
 
