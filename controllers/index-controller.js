cd638c9f686b6fab1125f35d17c5869eb33c54f7
Julia Nissen <nisse053@umn.edu>
2018-03-22 19:54:09 -0500

    usergroupInfo now being passed into index page

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -54,46 +54,11 @@ module.exports = function(app) {
 			}).then(function(result) {
 
 				indexObject["pranks"] = result;
-
-				res.json(indexObject)
-				// res.render('index', indexObject)
+				
+				res.render('index', indexObject)
 			})
 		});
 
 	});
-
-// 	db.hinder.findAll({
-// 		where: {
-// 			group_id: groupId
-// 		},
-// 		include: [{
-// 			model: db.hinder_category,
-// 			as: 'category',
-// 			required: true
-// 		},
-// 		{
-// 			model: db.user,
-// 			as: 'pranker',
-// 			required: true
-// 		},
-// 		{
-// 			model: db.user,
-// 			as: 'target',
-// 			required: true
-// 		}
-// 		]
-// 		// order: {
-// 		// 	['created_at', 'DESC']
-// 		// }
-// 	})
-// 		.then(function(pranks) {
-
-// 			// Send pranks object to handlebars
-// 			res.render('index', {
-// 				pranks: pranks
-// 			});
-			
-// 		});
-// });
 
 }
