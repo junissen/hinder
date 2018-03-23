ba76b2e065c10821bc3175c52b24feb5dd99448e
Julia Nissen <nisse053@umn.edu>
2018-03-22 21:32:33 -0500

    updated indexController

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -11,52 +11,98 @@ module.exports = function(app) {
 		var indexObject = {};
 
 		db.user.findAll({
+			attributes: ['id', ['user_name', 'name'], 'photo', 'group_id'],
 			where: {
 				id: userId
 			},
 			include: [{
+				attributes: [['id', 'group_id'], 'group_name', 'bio', 'photo'],
 				model: db.group,
 				as: 'group',
 				required: true
 			}]
 		}).then(function(result) {
 
-			console.log(result)
+			var user = {
+				'id': result[0].dataValues.id, 
+				'name': result[0].dataValues.name, 
+				'photo': result[0].dataValues.photo, 
+				'group_id': result[0].dataValues.group_id}
 
-			indexObject["userInfo"] = result;
+			indexObject["userInfo"] = user
 
-			var groupID = result[0].dataValues.group_id;
+			var group = result[0].dataValues.group.dataValues
 
-			
-			db.hinder.findAll({
+			indexObject["groupInfo"] = group
+
+			db.user.findAll({
+				attributes: ['id', ['user_name', 'name'], 'photo'],
 				where: {
-					group_id: groupID
-				},
-				include: [{
-					model: db.hinder_category,
-					as: "category",
-					required: true
-				},
-				{
-					model: db.user,
-					as: "pranker",
-					required: true
-				},
-				{
-					model: db.user,
-					as: "target",
-					required: true
-				}
-				]
-				// order: {
-				// 	['created_at', 'DESC']
-				// }
+					group_id: indexObject.userInfo.group_id,
+					id: {
+						[db.Sequelize.Op.ne]: userId
+					}
+				}	
 			}).then(function(result) {
 
-				indexObject["pranks"] = result;
-				
-				res.render('index', indexObject)
-			})
+				indexObject["group_members"] = []
+
+				for (var i = 0; i < result.length; i ++ ){
+
+					indexObject.group_members.push(result[i].dataValues)
+				}
+
+				db.hinder.findAll({
+					where: {
+						group_id: indexObject.userInfo.group_id
+					},
+					include: [{
+						model: db.hinder_category,
+						as: "category",
+						required: true
+					},
+					{
+						model: db.user,
+						as: "pranker",
+						required: true
+					},
+					{
+						model: db.user,
+						as: "target",
+						required: true
+					}
+					]
+					// order: {
+					// 	['created_at', 'DESC']
+					// }
+				}).then(function(result) {
+
+					var pranks = [];
+
+					for (var i = 0; i < result.length; i ++ ) {
+						var object = {
+							'id': result[i].dataValues.id,
+							'complete': result[i].dataValues.hinder_complete,
+							'thumbs_up': result[i].dataValues.thumbs_up,
+							'thumbs_down': result[i].dataValues.thumbs_down,
+							'hinder_info': {
+								'id': result[i].dataValues.category.id,
+								'hinder_type': result[i].dataValues.category.hinder_type,
+								'asset': result[i].dataValues.category.asset,
+								'created_at': result[i].dataValues.category.created_at
+							}
+						}
+
+						pranks.push(object)
+					}
+
+					indexObject["pranks"] = pranks;
+
+					res.render('index', indexObject)
+				})
+
+			});
+
 		});
 
 	});
